e56cde2373f5c93715056fca94c9ef78f6d50c6b
Riley <116728334+RClark707@users.noreply.github.com>
2025-05-16 05:56:47 -0400

    Phase and Holding Generators Refined and Complete

--- functions.js
+++ functions.js
@@ -1,548 +1,83 @@
 // ARRAY TO HOLD RANDOM VALUES
 let noise = [];
 
-// HEX SPARKS
-const feature1 = [
-    'Buried'
-    , 'Colourful'
-    , 'Adorned'
-    , 'Spiked'
-    , 'Split'
-    , 'Entombed'
-    , 'Reflective'
-    , 'Veiled'
-    , 'Hot'
-    , 'Drowned'
-    , 'Desecrated'
-    , 'Isolated'
-];
-const feature2 = [
-    'Brook'
-    , 'Seat'
-    , 'Pit'
-    , 'Cave'
-    , 'Monolith'
-    , 'Mound'
-    , 'Cairn'
-    , 'Pond'
-    , 'Waterfall'
-    , 'Spring'
-    , 'Arch'
-    , 'Henge'
-];
-const wonder1 = [
-    'Pleasure'
-    , 'Secrets'
-    , 'Prophecy'
-    , 'Healing'
-    , 'Desire'
-    , 'Memory'
-    , 'Death'
-    , 'Strength'
-    , 'Temptation'
-    , 'Pain'
-    , 'Regret'
-    , 'Time'
-];
-const wonder2 = [
-    'Light'
-    , 'Flames'
-    , 'Stones'
-    , 'Beasts'
-    , 'Sparks'
-    , 'Trails'
-    , 'Mist'
-    , 'Colours'
-    , 'Plants'
-    , 'Wind'
-    , 'Water'
-    , 'Shadows'
-];
-const landscape1 = [
-    'Barren'
-    , 'Dry'
-    , 'Grey'
-    , 'Sparse'
-    , 'Sharp'
-    , 'Teeming'
-    , 'Still'
-    , 'Soft'
-    , 'Overgrown'
-    , 'Vivid'
-    , 'Sodden'
-    , 'Lush'
-];
-const landscape2 = [
-    'Marsh'
-    , 'Heath'
-    , 'Crags'
-    , 'Peaks'
-    , 'Forest'
-    , 'Valley'
-    , 'Hills'
-    , 'Meadow'
-    , 'Bog'
-    , 'Lakes'
-    , 'Glades'
-    , 'Plain'
-];
-const otherworld1 = [
-    'Acidic'
-    , 'Black'
-    , 'Smoke'
-    , 'Frozen'
-    , 'Dead'
-    , 'Broken'
-    , 'Colossal'
-    , 'Living'
-    , 'Burning'
-    , 'Sludge'
-    , 'White'
-    , 'Sweet'
-];
-const otherworld2 = [
-    'Flats'
-    , 'Labyrinth'
-    , 'Ruins'
-    , 'Stairs'
-    , 'Desert'
-    , 'Craters'
-    , 'Cavern'
-    , 'Jungle'
-    , 'Dunes'
-    , 'Tunnels'
-    , 'Island'
-    , 'Mountain'
-];
-const weather1 = [
-    'Gentle'
-    , 'Fleeting'
-    , 'Persistent'
-    , 'Bright'
-    , 'Thin'
-    , 'Cool'
-    , 'Hot'
-    , 'Solid'
-    , 'Dull'
-    , 'Faint'
-    , 'Abundant'
-    , 'Harsh'
-];
-const weather2 = [
-    'Rain'
-    , 'Gusts'
-    , 'Cloud'
-    , 'Sunlight'
-    , 'Mist'
-    , 'Humidity'
-    , 'Thunder'
-    , 'Dust'
-    , 'Warmth'
-    , 'Drizzle'
-    , 'Breeze'
-    , 'Fog'
-];
-const sky1 = [
-    'Glittering'
-    , 'Violet'
-    , 'Sapphire'
-    , 'Pale'
-    , 'Fiery'
-    , 'Ivory'
-    , 'Slate'
-    , 'Pink'
-    , 'Golden'
-    , 'Bloody'
-    , 'Bright'
-    , 'Inky'
-];
-const sky2 = [
-    'Aurora'
-    , 'Haze'
-    , 'Marble'
-    , 'Glow'
-    , 'Billows'
-    , 'Swirl'
-    , 'Streaks'
-    , 'Dapple'
-    , 'Rays'
-    , 'Pillars'
-    , 'Shimmer'
-    , 'Swells'
-];
-const water1 = [
-    'Crystal'
-    , 'Teal'
-    , 'Pearlescent'
-    , 'Mucky'
-    , 'Cobalt'
-    , 'Verdant'
-    , 'Frosted'
-    , 'Dark'
-    , 'Verdigris'
-    , 'Silver'
-    , 'Emerald'
-    , 'Jade'
-];
-const water2 = [
-    'Silk'
-    , 'Ripples'
-    , 'Abyss'
-    , 'Churn'
-    , 'Froth'
-    , 'Mirror'
-    , 'Surge'
-    , 'Glass'
-    , 'Surf'
-    , 'Rapids'
-    , 'Spray'
-    , 'Bubbles'
-];
-const flora1 = [
-    'Aromatic'
-    , 'Ashen'
-    , 'Blooming'
-    , 'Twisted'
-    , 'Towering'
-    , 'Fruitful'
-    , 'Stinging'
-    , 'Vibrant'
-    , 'Brittle'
-    , 'Thorny'
-    , 'Sturdy'
-    , 'Resinous'
-];
-const flora2 = [
-    'Grasses'
-    , 'Heather'
-    , 'Shrubs'
-    , 'Brambles'
-    , 'Canopy'
-    , 'Ferns'
-    , 'Trunks'
-    , 'Vines'
-    , 'Conifers'
-    , 'Saplings'
-    , 'Reeds'
-    , 'Roots'
-];
-const fauna1 = [
-    'Watchful'
-    , 'Helpful'
-    , 'Graceful'
-    , 'Loud'
-    , 'Mischievous'
-    , 'Deceitful'
-    , 'Enlightening'
-    , 'Placid'
-    , 'Beautiful'
-    , 'Mighty'
-    , 'Hostile'
-    , 'Aloof'
-];
-const fauna2 = [
-    'Ungulates'
-    , 'Songbirds'
-    , 'Canines'
-    , 'Rodents'
-    , 'Amphibians'
-    , 'Insects'
-    , 'Felines'
-    , 'Reptiles'
-    , 'Hawks'
-    , 'Mustelids'
-    , 'Fowl'
-    , 'Bears'
-];
+// HEX SPARK MAP
+const hex_sparks = new Map([
+    ['feature1', ['Buried', 'Colourful', 'Adorned', 'Spiked', 'Split', 'Entombed', 'Reflective', 'Veiled', 'Hot', 'Drowned', 'Desecrated', 'Isolated']]
+    , ['feature2', ['Brook', 'Seat', 'Pit', 'Cave', 'Monolith', 'Mound', 'Cairn', 'Pond', 'Waterfall', 'Spring', 'Arch', 'Henge']]
+    , ['wonder1', ['Seat', 'Pit', 'Cave', 'Monolith', 'Mound', 'Cairn', 'Pond', 'Waterfall', 'Spring', 'Arch', 'Henge', 'Light']]
+    , ['wonder2', ['Light', 'Flames', 'Stones', 'Beasts', 'Sparks', 'Trails', 'Mist', 'Colours', 'Plants', 'Wind', 'Water', 'Shadows']]
+    , ['landscape1', ['Barren', 'Dry', 'Grey', 'Sparse', 'Sharp', 'Teeming', 'Still', 'Soft', 'Overgrown', 'Vivid', 'Sodden', 'Lush']]
+    , ['landscape2', ['Marsh', 'Heath', 'Crags', 'Peaks', 'Forest', 'Valley', 'Hills', 'Meadow', 'Bog', 'Lakes', 'Glades', 'Plain']]
+    , ['otherworld1', ['Acidic', 'Black', 'Smoke', 'Frozen', 'Dead', 'Broken', 'Colossal', 'Living', 'Burning', 'Sludge', 'White', 'Sweet']]
+    , ['otherworld2', ['Flats', 'Labyrinth', 'Ruins', 'Stairs', 'Desert', 'Craters', 'Cavern', 'Jungle', 'Dunes', 'Tunnels', 'Island', 'Mountain']]
+    , ['weather1', ['Gentle', 'Fleeting', 'Persistent', 'Bright', 'Thin', 'Cool', 'Hot', 'Solid', 'Dull', 'Faint', 'Abundant', 'Harsh']]
+    , ['weather2', ['Fleeting', 'Persistent', 'Bright', 'Thin', 'Cool', 'Hot', 'Solid', 'Dull', 'Faint', 'Abundant', 'Harsh', 'Aromatic']]
+    , ['sky1', ['Glittering', 'Violet', 'Sapphire', 'Pale', 'Fiery', 'Ivory', 'Slate', 'Pink', 'Golden', 'Bloody', 'Bright', 'Inky']]
+    , ['sky2', ['Violet', 'Sapphire', 'Pale', 'Fiery', 'Ivory', 'Slate', 'Pink', 'Golden', 'Bloody', 'Bright', 'Inky', 'Crystal']]
+    , ['water1', ['Sapphire', 'Pale', 'Fiery', 'Ivory', 'Slate', 'Pink', 'Golden', 'Bloody', 'Bright', 'Inky', 'Crystal', 'Teal']]
+    , ['water2', ['Silk', 'Ripples', 'Abyss', 'Churn', 'Froth', 'Mirror', 'Surge', 'Glass', 'Surf', 'Rapids', 'Spray', 'Bubbles']]
+    , ['flora1', ['Aromatic', 'Ashen', 'Blooming', 'Twisted', 'Towering', 'Fruitful', 'Stinging', 'Vibrant', 'Brittle', 'Thorny', 'Sturdy', 'Resinous']]
+    , ['flora2', ['Grasses', 'Heather', 'Shrubs', 'Brambles', 'Canopy', 'Ferns', 'Trunks', 'Vines', 'Conifers', 'Saplings', 'Reeds', 'Roots']]
+    , ['fauna1', ['Watchful', 'Helpful', 'Graceful', 'Loud', 'Mischievous', 'Deceitful', 'Enlightening', 'Placid', 'Beautiful', 'Mighty', 'Hostile', 'Aloof']]
+    , ['fauna2', ['Ungulates', 'Songbirds', 'Canines', 'Rodents', 'Amphibians', 'Insects', 'Felines', 'Reptiles', 'Hawks', 'Mustelids', 'Fowl', 'Bears']]
+]);
+
+// HOLDING SPARK MAP
+const holding_sparks = new Map([
+    ['bailey1', ['Filthy', 'Abandoned', 'Joyous', 'Sophisticated', 'Industrious', 'Humble', 'Majestic', 'Hallowed', 'Rustic', 'Solemn', 'Bustling', 'Immaculate']]
+    , ['bailey2', ['Marketplace', 'Forge', 'Library', 'Fountain', 'Temple', 'Forum', 'Tomb', 'Garden', 'Hall', 'Workshops', 'Arena', 'Garrison']]
+    , ['drama1', ['Betrayal', 'Jealousy', 'Rivalry', 'Infidelity', 'Coup', 'Ambition', 'Redemption', 'Revelation', 'Wrath', 'Greed', 'Banishment', 'Manipulation']]
+    , ['drama2', ['Brawl', 'Poison', 'Oath', 'Feast', 'Letters', 'Disguise', 'Inheritance', 'Assassin', 'Family', 'Alcohol', 'Blackmail', 'Gold']]
+    , ['food1', ['Spiced', 'Herbal', 'Crunchy', 'Sour', 'Dry', 'Fermented', 'Salted', 'Wet', 'Fatty', 'Chewy', 'Sweet', 'Mild']]
+    , ['food2', ['Fish', 'Fruit', 'Stew', 'Mushrooms', 'Pie', 'Cheese', 'Nuts', 'Cake', 'Porridge', 'Bread', 'Vegetable', 'Meat']]
+    , ['goods1', ['Military', 'Abundant', 'Traditional', 'Specialist', 'Industrious', 'Innovative', 'Secretive', 'Simple', 'Strong', 'Decorated', 'Fine', 'Lucky']]
+    , ['goods2', ['Textile', 'Livestock', 'Grain', 'Mead', 'Tools', 'Stone', 'Wood', 'Pottery', 'Metal', 'Leather', 'Honey', 'Herb']]
+    , ['holding1', ['Dark', 'Ruined', 'Hostile', 'Ancient', 'Ornate', 'Wild', 'Pristine', 'Fortified', 'Unfinished', 'Welcoming', 'Proud', 'Bright']]
+    , ['holding2', ['Turrets', 'Tower', 'Wall', 'Battlements', 'Citadel', 'Gate', 'Spire', 'Dome', 'Beacons', 'Bridge', 'Pillars', 'Moat']]
+    , ['keep1', ['Hearth', 'Throne', 'Musicians', 'Pool', 'Advisers', 'Servants', 'Shrine', 'Table', 'Reliquary', 'Cauldron', 'Chandelier', 'Guards']]
+    , ['keep2', ['Garrison', 'Antlers', 'Silver', 'Heraldry', 'Bones', 'Flowers', 'Scripture', 'Jewels', 'Wreaths', 'Candles', 'Fur', 'Tapestries', 'Shields']]
+    , ['luxuries1', ['Antique', 'Intricate', 'Unique', 'Scarce', 'Hazardous', 'Flawless', 'Luminous', 'Lost', 'Esoteric', 'Sacred', 'Mythical', 'Beautiful']]
+    , ['luxuries2', ['Jewel', 'Wine', 'Spice', 'Fragrance', 'Silk', 'Fur', 'Artwork', 'Sword', 'Creature', 'Ore', 'Root', 'Scripture']]
+    , ['news1', ['Duel', 'Birth', 'Market', 'Trial', 'Ritual', 'Mercenaries', 'Festival', 'Tournament', 'Punishment', 'Performance', 'Death', 'Marriage']]
+    , ['news2', ['Pensive', 'Joyous', 'Content', 'Divided', 'Furious', 'Sceptical', 'Adoring', 'Nostalgic', 'Unified', 'Bleak', 'Solemn', 'Optimistic']]
+    , ['woe1', ['Secretive', 'Violent', 'Looming', 'Sudden', 'Ongoing', 'Prophecised', 'Mysterious', 'Sanctioned', 'Unseen', 'Vast', 'Escalating', 'Concealed']]
+    , ['woe2', ['Disease', 'Famine', 'Raids', 'Invasion', 'Abduction', 'Storm', 'Fire', 'Revolt', 'Exodus', 'Beast', 'Killing', 'Theft']]
+]);
 
 // FUNCTION TO ROLL RANDOM MYTH PHASE
 function nextPhase() {
-    for (i = 0; i < 18; i++) {
-        noise[i] = Math.floor(Math.random() * 12);
+    // ROLL FOR MYTH ENCOUNTER
+    let roll6 = Math.floor(Math.random() * 6) + 1;
+    // ASSIGN MYTH TEXT
+    switch (roll6) {
+        case 1:
+            // ENCOUNTER RANDOM MYTH
+            roll6 = Math.floor(Math.random() * 6) + 1;
+            setInner('myths', 'The Company encounters a random Myth: #' + roll6 + '.');
+            break;
+        case 2 || 3:
+            // ENCOUNTER NEAREST MYTH
+            setInner('myths', 'The Company encounters the nearest Myth.');
+            break;
+        default:
+            // ENCOUNTER LANDMARK
+            setInner('myths', 'The Company encounters this hex\'s Landmark.');
+            break;
     }
 
-    return null
+    // ROLL AND ASSIGN NATURE FEATURES
+    for (let [key, value] of hex_sparks) {
+        setInner(key, value[Math.floor(Math.random() * 12)]);
+    }
 }
 
-// HOLDING SPARKS
-const h_styles = [
-    'Dark'
-    , 'Ruined'
-    , 'Hostile'
-    , 'Ancient'
-    , 'Ornate'
-    , 'Wild'
-    , 'Pristine'
-    , 'Fortified'
-    , 'Unfinished'
-    , 'Welcoming'
-    , 'Proud'
-    , 'Bright'
-];
-const h_features = [
-    'Turrets'
-    , 'Tower'
-    , 'Wall'
-    , 'Battlements'
-    , 'Citadel'
-    , 'Gate'
-    , 'Spire'
-    , 'Dome'
-    , 'Beacons'
-    , 'Bridge'
-    , 'Pillars'
-    , 'Moat'
-];
-const b_styles = [
-    'Filthy'
-    , 'Abandoned'
-    , 'Joyous'
-    , 'Sophisticated'
-    , 'Industrious'
-    , 'Humble'
-    , 'Majestic'
-    , 'Hallowed'
-    , 'Rustic'
-    , 'Solemn'
-    , 'Bustling'
-    , 'Immaculate'
-];
-const b_features = [
-    'Marketplace'
-    , 'Forge'
-    , 'Library'
-    , 'Fountain'
-    , 'Temple'
-    , 'Forum'
-    , 'Tomb'
-    , 'Garden'
-    , 'Hall'
-    , 'Workshops'
-    , 'Arena'
-    , 'Garrison'
-];
-const k_centers = [
-    'Hearth'
-    , 'Throne'
-    , 'Musicians'
-    , 'Pool'
-    , 'Advisers'
-    , 'Servants'
-    , 'Shrine'
-    , 'Table'
-    , 'Reliquary'
-    , 'Cauldron'
-    , 'Chandelier'
-    , 'Guards'
-];
-const k_decor = [
-    'Antlers'
-    , 'Silver'
-    , 'Heraldry'
-    , 'Bones'
-    , 'Flowers'
-    , 'Scripture'
-    , 'Jewles'
-    , 'Wreaths'
-    , 'Candles'
-    , 'Fur'
-    , 'Tapestries'
-    , 'Shields'
-];
-const f_qual = [
-    'Spiced'
-    , 'Herbal'
-    , 'Crunchy'
-    , 'Sour'
-    , 'Dry'
-    , 'Fermented'
-    , 'Salted'
-    , 'Salted'
-    , 'Wet'
-    , 'Fatty'
-    , 'Chewy'
-    , 'Sweet'
-    , 'Mild'
-];
-const f_type = [
-    'Fish'
-    , 'Fruit'
-    , 'Stew'
-    , 'Mushrooms'
-    , 'Pie'
-    , 'Cheese'
-    , 'Nuts'
-    , 'Cake'
-    , 'Porridge'
-    , 'Bread'
-    , 'Vegetable'
-    , 'Meat'
-];
-const g_theme = [
-    'Military'
-    , 'Abundant'
-    , 'Traditional'
-    , 'Specialist'
-    , 'Industrious'
-    , 'Innovative'
-    , 'Secretive'
-    , 'Simple'
-    , 'Simple'
-    , 'Strong'
-    , 'Decorated'
-    , 'Fine'
-    , 'Lucky'
-];
-const g_type = [
-    'Textile'
-    , 'Livestock'
-    , 'Grain'
-    , 'Mead'
-    , 'Tools'
-    , 'Stone'
-    , 'Wood'
-    , 'Pottery'
-    , 'Metal'
-    , 'Leather'
-    , 'Honey'
-    , 'Herb'
-];
-const l_rarity = [
-    'Antique'
-    , 'Intricate'
-    , 'Unique'
-    , 'Scarce'
-    , 'Hazardous'
-    , 'Flawless'
-    , 'Luminous'
-    , 'Lost'
-    , 'Esoteric'
-    , 'Sacred'
-    , 'Mythical'
-    , 'Beautiful'
-];
-const l_type = [
-    'Jewel'
-    , 'Wine'
-    , 'Spice'
-    , 'Fragrance'
-    , 'Silk'
-    , 'Fur'
-    , 'Artwork'
-    , 'Sword'
-    , 'Creature'
-    , 'Ore'
-    , 'Root'
-    , 'Scripture'
-];
-const d_theme = [
-    'Betrayal'
-    , 'Jealousy'
-    , 'Rivalry'
-    , 'Infidelity'
-    , 'Coup'
-    , 'Ambition'
-    , 'Redemption'
-    , 'Revelation'
-    , 'Wrath'
-    , 'Greed'
-    , 'Banishment'
-    , 'Manipulation'
-];
-const d_detail = [
-    'Brawl'
-    , 'Poison'
-    , 'Oath'
-    , 'Feast'
-    , 'Letters'
-    , 'Disguise'
-    , 'Inheritance'
-    , 'Assassin'
-    , 'Family'
-    , 'Alcohol'
-    , 'Blackmail'
-    , 'Gold'
-];
-const w_desc = [
-    'Secretive'
-    , 'Violent'
-    , 'Looming'
-    , 'Sudden'
-    , 'Ongoing'
-    , 'Prophecied'
-    , 'Mysterious'
-    , 'Sanctioned'
-    , 'Unseen'
-    , 'Vast'
-    , 'Escalating'
-    , 'Concealed'
-];
-const w_incident = [
-    'Disease'
-    , 'Famine'
-    , 'Raids'
-    , 'Invasion'
-    , 'Abduction'
-    , 'Storm'
-    , 'Fire'
-    , 'Revolt'
-    , 'Exodus'
-    , 'Beast'
-    , 'Killing'
-    , 'Theft'
-];
-const n_subj = [
-    'Duel'
-    , 'Birth'
-    , 'Market'
-    , 'Trial'
-    , 'Ritual'
-    , 'Mercenaries'
-    , 'Festival'
-    , 'Tournament'
-    , 'Punishment'
-    , 'Performance'
-    , 'Death'
-    , 'Marriage'
-];
-const n_mood = [
-    'Pensive'
-    , 'Joyous'
-    , 'Content'
-    , 'Divided'
-    , 'Furious'
-    , 'Skeptical'
-    , 'Adoring'
-    , 'Nostalgic'
-    , 'Unified'
-    , 'Bleak'
-    , 'Solemn'
-    , 'Optimistic'
-];
-
 function generateHolding() {
-    for (i = 0; i < 18; i++) {
-        noise[i] = Math.floor(Math.random() * 12);
-        if (noise[i] == 12) {
-            print("I'm here!")
-        }
+    for (let [key, value] of holding_sparks) {
+        setInner(key, value[Math.floor(Math.random() * 12)]);
     }
-    document.getElementById('h-style').innerHTML = h_styles[noise[0]];
-    document.getElementById('h-feature').innerHTML = h_features[noise[1]];
-    document.getElementById('b-style').innerHTML = b_styles[noise[2]];
-    document.getElementById('b-feature').innerHTML = b_features[noise[3]];
-    document.getElementById('k-center').innerHTML = k_centers[noise[4]];
-    document.getElementById('k-decor').innerHTML = k_decor[noise[5]];
-    document.getElementById('f-quality').innerHTML = f_qual[noise[6]];
-    document.getElementById('f-type').innerHTML = f_type[noise[7]];
-    document.getElementById('g-theme').innerHTML = g_theme[noise[8]];
-    document.getElementById('g-type').innerHTML = g_type[noise[9]];
-    document.getElementById('l-rarity').innerHTML = l_rarity[noise[10]];
-    document.getElementById('l-type').innerHTML = l_type[noise[11]];
-    document.getElementById('d-theme').innerHTML = d_theme[noise[12]];
-    document.getElementById('d-detail').innerHTML = d_detail[noise[13]];
-    document.getElementById('w-desc').innerHTML = w_desc[noise[14]];
-    document.getElementById('w-incident').innerHTML = w_incident[noise[15]];
-    document.getElementById('n-subject').innerHTML = n_subj[noise[16]];
-    document.getElementById('n-mood').innerHTML = n_mood[noise[17]];
+}
+
+function setInner(element_id, text) {
+    document.getElementById(element_id).innerHTML = text;
 }
